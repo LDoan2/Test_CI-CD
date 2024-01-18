1d7e92c42bc6b9194896c61dccdf775fad6130a7
DOAN Lancelot <lancelot.doan9@etu.univ-lorraine.fr>
2024-01-18 09:46:01 +0100

    composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,7 +7,10 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'PrivateBin\\' => array($baseDir . '/lib'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Jdenticon\\' => array($vendorDir . '/jdenticon/jdenticon/src'),
     'Identicon\\' => array($vendorDir . '/yzalis/identicon/src/Identicon'),
     'IPLib\\' => array($vendorDir . '/mlocati/ip-lib/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
